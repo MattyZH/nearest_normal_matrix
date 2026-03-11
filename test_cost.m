1b6baa8978f3ef56ec82bacaa98866e89eb2df5c
matveizhukov <matzhukov2000@gmail.com>
2026-03-11 12:01:04 +0200

    deleted redundant files

--- test_cost.m
+++
@@ -1,7 +0,0 @@
-function tc = test_cost(alpha, gamma, A, beta, delta)
-Q = exp(-1i*alpha) * [exp(1i / 2  * (-delta - beta))*cos(gamma) -exp(1i / 2  * (delta - beta))*sin(gamma) ;
-    exp(1i / 2  * (-delta + beta))*sin(gamma) exp(1i / 2  * (delta + beta))*cos(gamma)];
-B = Q' * A * Q;
-S = B - diag(diag(B));
-tc = norm(B' * S - S * B', 'fro')^2;
-end
